0f9f8cf57c9a7860b940bd4bf5c0e48fa891972d
= <rohitbhatwara1@gmail.com>
2023-12-10 17:48:40 +0530

    feat(navlink): add active state on navlink

--- src/app/(dashboard)/builder/[formId]/settings/_components/nav-link.tsx
+++ src/app/(dashboard)/builder/[formId]/settings/_components/nav-link.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import Link, { LinkProps } from 'next/link'
 import { cloneElement } from 'react'
+import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
 
 type NavLinkProps = LinkProps & {
@@ -9,8 +12,14 @@ type NavLinkProps = LinkProps & {
 }
 
 export default function NavLink({ href, icon, title, description }: NavLinkProps) {
+  const pathname = usePathname()
+  const isActive = pathname.includes(href as string)
+
   return (
-    <Link href={href} className={cn('flex items-center gap-2 border-b px-4 py-2 hover:bg-secondary')}>
+    <Link
+      href={href}
+      className={cn('flex items-center gap-2 border-b px-4 py-2 hover:bg-secondary', { 'bg-secondary': isActive })}
+    >
       <div>{cloneElement(icon, { className: 'w-4 h-4 text-primary' })}</div>
       <div>
         <h3 className="text-sm">{title}</h3>
